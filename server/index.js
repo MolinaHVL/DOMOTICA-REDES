3e033fd6618a10c35dea1d11d68fc79df57bf83b
MolinaHVL <christopher.molina@cetys.edu.mx>
2023-06-05 14:06:31 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var server = require('https').Server(app);
+var server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 app.use(express.static('public'));
